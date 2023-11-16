2c0c4d639aa5212b147bb12ccfe44b19e9f98eb8
Rafael H <nevesrafel@hotmail.com>
2023-11-15 21:59:34 -0300

    #refact: add migrations

---
+++ Migrations/20231116005838_RetornoEstadoAntigo.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CamposDealerWebProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class RetornoEstadoAntigo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vendas_Clientes_ClienteIdCliente",
+                table: "Vendas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vendas_ClienteIdCliente",
+                table: "Vendas");
+
+            migrationBuilder.DropColumn(
+                name: "ClienteIdCliente",
+                table: "Vendas");
+
+            migrationBuilder.RenameColumn(
+                name: "IdCliente",
+                table: "Vendas",
+                newName: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_ClienteId",
+                table: "Vendas",
+                column: "ClienteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vendas_Clientes_ClienteId",
+                table: "Vendas",
+                column: "ClienteId",
+                principalTable: "Clientes",
+                principalColumn: "IdCliente",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vendas_Clientes_ClienteId",
+                table: "Vendas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vendas_ClienteId",
+                table: "Vendas");
+
+            migrationBuilder.RenameColumn(
+                name: "ClienteId",
+                table: "Vendas",
+                newName: "IdCliente");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClienteIdCliente",
+                table: "Vendas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_ClienteIdCliente",
+                table: "Vendas",
+                column: "ClienteIdCliente");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vendas_Clientes_ClienteIdCliente",
+                table: "Vendas",
+                column: "ClienteIdCliente",
+                principalTable: "Clientes",
+                principalColumn: "IdCliente");
+        }
+    }
+}
